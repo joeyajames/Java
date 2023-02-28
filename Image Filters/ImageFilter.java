831ba81aa818cd37278a3755f29dc2d4a17f818d
Joe James <joeyajames@yahoo.com>
2023-02-28 15:56:45 -0800

    added heavyBlur and detectEdges functions.

--- Image Filters/ImageFilter.java
+++ Image Filters/ImageFilter.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * Filter Algorithms for Java BufferedImage
+ * (c) 2023, Joe James
+ * 
+ * This program loads an image file. It has methods to:  
+ *  - display the image in a JPanel popup window; 
+ *  - convert the image to grayscale;
+ *  - scale a grayscale image to a different size;
+ *  - pixelate a grayscale image;
+ *  - apply a Gaussian blur to a grayscale image;
+ *  - detect edges of a grayscale image;
+ *  - brighten a color image.
+ * 
+ * These files are mainly intended to accompany my series of YouTube tutorial 
+ * videos here, https://www.youtube.com/user/joejamesusa and are mainly 
+ * intended for educational purposes. You are invited to subscribe to my 
+ * video channel, and to download and use any code in this Java 
+ * repository, according to the MIT License. Feel free to post any comments 
+ * on my YouTube channel. 
+ * 
+ * ****************************************************************************/
+
 import java.util.*;
 import java.awt.*;
 import java.awt.image.*;
@@ -10,20 +32,25 @@ public class ImageFilter {
 
 	public static void main(String[] args) {
 		File file = new File("photo.png");
+		//File file = new File("License Plate Photos/ca_10.jpeg");
 		BufferedImage img = null;
 
 		try { img = ImageIO.read(file); } 
 		catch (IOException e) { e.printStackTrace(System.out); }
 
 		if (img != null) {
-			//display(img);
+			display(img);
+			//img = brighten(img);
 			img = toGrayScale(img);
 			//img = toGrayScale2(img);
-			display(img);
+			//display(img);
 			//img = pixelate(img);
 			//img = pixelate2(img, 3);
 			//img = resize(img, 150);
-			img = blur(img);
+			//img = blur(img);
+			img = blur(blur(img));
+			img = heavyblur(img);
+			img = detectEdges(img);
 			display(img);
 		}
 	}
@@ -130,7 +157,7 @@ public class ImageFilter {
 		return scaleOp.filter(img, scaledImg);
 	}
 
-	// apply Gaussian blur to a grayscale image
+	// apply 3x3 Gaussian blur to a grayscale image
 	public static BufferedImage blur (BufferedImage img) {
 		BufferedImage blurImg = new BufferedImage(
 			img.getWidth()-2, img.getHeight()-2, BufferedImage.TYPE_BYTE_GRAY);
@@ -151,6 +178,93 @@ public class ImageFilter {
 			}
 		}
 		return blurImg;
+	}
+
+	// apply 5x5 Gaussian blur to a grayscale image
+	public static BufferedImage heavyblur (BufferedImage img) {
+		BufferedImage blurImg = new BufferedImage(
+			img.getWidth()-4, img.getHeight()-4, BufferedImage.TYPE_BYTE_GRAY);
+		int pix = 0;
+		for (int y=0; y<blurImg.getHeight(); y++) {
+			for (int x=0; x<blurImg.getWidth(); x++) {
+				pix = (int)(
+				10*(img.getRGB(x+3, y+3)& 0xFF)
+				+ 6*(img.getRGB(x+2, y+1)& 0xFF)
+				+ 6*(img.getRGB(x+1, y+2)& 0xFF)
+				+ 6*(img.getRGB(x+2, y+3)& 0xFF)
+				+ 6*(img.getRGB(x+3, y+2)& 0xFF)
+				+ 4*(img.getRGB(x+1, y+1)& 0xFF)
+				+ 4*(img.getRGB(x+1, y+3)& 0xFF)
+				+ 4*(img.getRGB(x+3, y+1)& 0xFF)
+				+ 4*(img.getRGB(x+3, y+3)& 0xFF)
+				+ 2*(img.getRGB(x, y+1)& 0xFF)
+				+ 2*(img.getRGB(x, y+2)& 0xFF)
+				+ 2*(img.getRGB(x, y+3)& 0xFF)
+				+ 2*(img.getRGB(x+4, y+1)& 0xFF)
+				+ 2*(img.getRGB(x+4, y+2)& 0xFF)
+				+ 2*(img.getRGB(x+4, y+3)& 0xFF)
+				+ 2*(img.getRGB(x+1, y)& 0xFF)
+				+ 2*(img.getRGB(x+2, y)& 0xFF)
+				+ 2*(img.getRGB(x+3, y)& 0xFF)
+				+ 2*(img.getRGB(x+1, y+4)& 0xFF)
+				+ 2*(img.getRGB(x+2, y+4)& 0xFF)
+				+ 2*(img.getRGB(x+3, y+4)& 0xFF)
+				+ (img.getRGB(x, y)& 0xFF)
+				+ (img.getRGB(x, y+2)& 0xFF)
+				+ (img.getRGB(x+2, y)& 0xFF)
+				+ (img.getRGB(x+2, y+2)& 0xFF))/74;
+				int p = (255<<24) | (pix<<16) | (pix<<8) | pix; 
+				blurImg.setRGB(x,y,p);
+			}
+		}
+		return blurImg;
+	}
+
+	// detect edges of a grayscale image using Sobel algorithm 
+	// (for best results, apply blur before finding edges)
+	public static BufferedImage detectEdges (BufferedImage img) {
+		int h = img.getHeight(), w = img.getWidth(), threshold=30, p = 0;
+		BufferedImage edgeImg = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+		int[][] vert = new int[w][h];
+		int[][] horiz = new int[w][h];
+		int[][] edgeWeight = new int[w][h];
+		for (int y=1; y<h-1; y++) {
+			for (int x=1; x<w-1; x++) {
+				vert[x][y] = (int)(img.getRGB(x+1, y-1)& 0xFF + 2*(img.getRGB(x+1, y)& 0xFF) + img.getRGB(x+1, y+1)& 0xFF
+					- img.getRGB(x-1, y-1)& 0xFF - 2*(img.getRGB(x-1, y)& 0xFF) - img.getRGB(x-1, y+1)& 0xFF);
+				horiz[x][y] = (int)(img.getRGB(x-1, y+1)& 0xFF + 2*(img.getRGB(x, y+1)& 0xFF) + img.getRGB(x+1, y+1)& 0xFF
+					- img.getRGB(x-1, y-1)& 0xFF - 2*(img.getRGB(x, y-1)& 0xFF) - img.getRGB(x+1, y-1)& 0xFF);
+				edgeWeight[x][y] = (int)(Math.sqrt(vert[x][y] * vert[x][y] + horiz[x][y] * horiz[x][y]));
+				if (edgeWeight[x][y] > threshold)
+					p = (255<<24) | (255<<16) | (255<<8) | 255;
+				else 
+					p = (255<<24) | (0<<16) | (0<<8) | 0; 
+				edgeImg.setRGB(x,y,p);
+			}
+		}
+		return edgeImg;
+	}
+
+	// brighten color image by a percentage 
+	public static BufferedImage brighten (BufferedImage img, int percentage) {
+		int r=0, g=0, b=0, rgb=0, p=0;
+		int amount = (int)(percentage * 255 / 100); // rgb scale is 0-255, so 255 is 100%
+		BufferedImage newImage = new BufferedImage(
+			img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		for (int y=0; y<img.getHeight(); y+=1) {
+			for (int x=0; x<img.getWidth(); x+=1) {
+				rgb = img.getRGB(x, y);
+				r = ((rgb >> 16) & 0xFF) + amount;
+				g = ((rgb >> 8) & 0xFF) + amount;
+				b = (rgb & 0xFF) + amount;
+				if (r>255) r=255;
+				if (g>255) g=255;
+				if (b>255) b=255;
+				p = (255<<24) | (r<<16) | (g<<8) | b;
+				newImage.setRGB(x,y,p);
+			}
+		}
+		return newImage;
 	}
 
 }
