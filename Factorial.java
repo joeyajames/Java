4c445ecaa0122f4a873cc8b755b3d6654e50eddb
Shivansh <121636928+Vasu004@users.noreply.github.com>
2023-09-07 00:08:45 +0530

    Vasu Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,21 +1,33 @@
 public class Factorial {
-	public static void main(String[] args) {
-		Factorial factorial = new Factorial();
-		System.out.println(factorial.getRecursiveFactorial(6));
-		System.out.println(factorial.getIterativeFactorial(6));
-	}
-	
-	public int getRecursiveFactorial(int n) {
-		if (n < 0) return -1;
-		else if (n < 2) return 1;
-		else return (n * getRecursiveFactorial(n-1));		
-	}
-	
-	public int getIterativeFactorial(int n) {
-		if (n < 0) return -1;
-		int fact = 1;
-		for (int i = 1; i <= n; i++)
-			fact *= i;
-		return fact;
-	}
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        int n = 6; // You can change this to calculate factorial for a different number.
+        System.out.println("Recursive factorial of " + n + ": " + factorial.getRecursiveFactorial(n));
+        System.out.println("Iterative factorial of " + n + ": " + factorial.getIterativeFactorial(n));
+    }
+
+    public int getRecursiveFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+        }
+        return factorialRecursive(n);
+    }
+
+    private int factorialRecursive(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorialRecursive(n - 1);
+    }
+
+    public int getIterativeFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+        }
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 }
