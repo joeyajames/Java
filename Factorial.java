91b2c6079f953d97be9611ea7a2ad3f34dc7b967
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2023-12-15 11:06:06 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,5 @@
 public class Factorial {
 	public static void main(String[] args) {
-		Factorial factorial = new Factorial();
-		System.out.println(factorial.getRecursiveFactorial(6));
 		System.out.println(factorial.getIterativeFactorial(6));
 	}
 	
